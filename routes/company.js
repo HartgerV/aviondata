94b6f1fa9d631a7a19e4f280cb043e7ef8202bab
H.Veeman <h.veeman@st.hanze.nl>
2017-03-27 15:02:36 +0300

    Better error handling

--- routes/company.js
+++ routes/company.js
@@ -5,16 +5,26 @@ module.exports = function (router) {
             var isCompanyIdRegex = /\d{7}[-]\d{1}/;
             if(isCompanyIdRegex.exec(req.params.query)) {
                 request('http://avoindata.prh.fi/bis/v1/'+req.params.query, function (error, response, body) {
-                    console.log('error:', error);
-                    console.log('statusCode:', response && response.statusCode);
-                    res.status(response.statusCode).send(body);
+                    if(error) {
+                        console.log('error:', error);
+                        res.status(500).send(body);
+                    }
+                    else {
+                        console.log('statusCode:', response && response.statusCode);
+                        res.status(response.statusCode).send(body);
+                    }
                 });
             }
             else {
                 request('http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=10&resultsFrom=0&name=' + req.params.query, function (error, response, body) {
-                    console.log('error:', error);
-                    console.log('statusCode:', response && response.statusCode);
-                    res.status(response.statusCode).send(body);
+                    if(error) {
+                        console.log('error:', error);
+                        res.status(500).send(body);
+                    }
+                    else {
+                        console.log('statusCode:', response && response.statusCode);
+                        res.status(response.statusCode).send(body);
+                    }
                 });
             }
         });
